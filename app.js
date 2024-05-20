9fcc48267dd5e4a131650e3e15c48f574145edcf
vamsimohanyacham <142957313+vamsimohanyacham@users.noreply.github.com>
2024-05-20 20:10:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.post('/signin/', async (request, response) => {
 })
 
 app.post('/login/', async (req, res) => {
-  const {name, password} = req.body
+  const {name, password} = req.query
 
   if (!name || !password) {
     return res.status(400).json({error: 'Name and password are required'})
